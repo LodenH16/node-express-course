f22c5735826047a7b4b299ec1ed0bc9b1ea8403a
Thomas Harris <loden16harris@gmail.com>
2021-09-13 14:18:11 -0500

    add login POST route

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.json());
 
 const mockUserData = [{ name: "Mark" }, { name: "Jill" }];
 app.get("/users", function (req, res) {
@@ -18,6 +20,28 @@ app.get("/users/:id", function (req, res) {
     user: req.params.id,
   });
 });
+
+app.post("/login", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const mockUsername = "billyTheKid";
+  const mockPassword = "superSecret";
+
+  if (username === mockUsername && password === mockPassword) {
+    res.json({
+      success: true,
+      message: "password and username match!",
+      token: "encrypted token goes here",
+    });
+  } else {
+    res.json({
+      success: false,
+      mesage: "password and username do not match",
+    });
+  }
+});
+
 app.listen(8000, function () {
   console.log("server is running");
 });
